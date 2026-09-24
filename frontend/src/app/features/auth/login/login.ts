8ca3db33b4187d3bfe20508daad3a417693d5b42
Aditya010102 <adityatiw9525@gmail.com>
2026-09-24 14:16:33 +0530

    feat: add reactive login page

---
+++ frontend/src/app/features/auth/login/login.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../../core/auth/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.scss'
+})
+export class LoginComponent {
+
+  isLoading = false;
+  errorMessage = '';
+
+  loginForm = new FormGroup({
+    email: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.email
+      ]
+    }),
+
+    password: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(6)
+      ]
+    })
+  });
+
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router
+  ) { }
+
+  submit(): void {
+    this.errorMessage = '';
+
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    this.isLoading = true;
+
+    this.authService
+      .login(this.loginForm.getRawValue())
+      .subscribe({
+        next: () => {
+          this.isLoading = false;
+          console.log('Login successful');
+          //  this.router.navigate(['/dashboard']);
+        },
+
+        error: () => {
+          this.isLoading = false;
+          this.errorMessage = 'Invalid email or password.';
+        }
+      });
+  }
+}
