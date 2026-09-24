37c7e9c786286924bb52e35021a5dc27e54a2993
Aditya010102 <adityatiw9525@gmail.com>
2026-09-24 22:33:03 +0530

    feat: connect issues page to backend

--- frontend/src/app/features/issues/issues.ts
+++ frontend/src/app/features/issues/issues.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {
+    ChangeDetectorRef,
+    Component,
+    OnInit
+} from '@angular/core';
+
+import { IssueService } from './services/issue';
+import { IssueResponse } from '../../core/models/issue-response';
 
 @Component({
     selector: 'app-issues',
@@ -6,5 +13,56 @@ import { Component } from '@angular/core';
     templateUrl: './issues.html',
     styleUrl: './issues.scss'
 })
-export class IssuesComponent {
+export class IssuesComponent implements OnInit {
+
+    issues: IssueResponse[] = [];
+
+    isLoading = false;
+
+    errorMessage = '';
+
+    constructor(
+        private readonly issueService: IssueService,
+        private readonly changeDetectorRef: ChangeDetectorRef
+    ) { }
+
+    ngOnInit(): void {
+        this.loadIssues();
+    }
+
+    loadIssues(): void {
+
+        this.isLoading = true;
+        this.errorMessage = '';
+
+        this.issueService
+            .getIssues()
+            .subscribe({
+
+                next: response => {
+
+                    this.issues = response.content;
+
+                    this.isLoading = false;
+
+                    this.changeDetectorRef.detectChanges();
+                },
+
+                error: error => {
+
+                    console.error(
+                        'Failed to load issues:',
+                        error
+                    );
+
+                    this.errorMessage =
+                        'Unable to load issues. Please try again.';
+
+                    this.isLoading = false;
+
+                    this.changeDetectorRef.detectChanges();
+                }
+
+            });
+    }
 }
