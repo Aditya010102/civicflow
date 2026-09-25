500e431bc663d7f6b936e9f811c784f5df7559da
Aditya010102 <adityatiw9525@gmail.com>
2026-09-25 22:39:20 +0530

    Add Kafka dead letter topic recovery

--- backend/core-java/src/main/java/com/civicflow/core/config/KafkaConsumerConfig.java
+++ backend/core-java/src/main/java/com/civicflow/core/config/KafkaConsumerConfig.java
@@ -1,15 +1,23 @@
 package com.civicflow.core.config;
 
 import com.civicflow.core.event.IssueEvent;
+
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
+import org.springframework.kafka.core.KafkaTemplate;
+
 import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+
+import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
+
 import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.HashMap;
@@ -66,27 +74,39 @@ public class KafkaConsumerConfig {
         );
     }
 
+
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, IssueEvent>
-    issueEventKafkaListenerContainerFactory() {
+    issueEventKafkaListenerContainerFactory(
+            ConsumerFactory<String, IssueEvent> consumerFactory,
+            DefaultErrorHandler kafkaErrorHandler
+    ) {
 
         ConcurrentKafkaListenerContainerFactory<String, IssueEvent>
                 factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
 
         factory.setConsumerFactory(
-                issueEventConsumerFactory()
+                consumerFactory
         );
 
         factory.setCommonErrorHandler(
-                kafkaErrorHandler()
+                kafkaErrorHandler
         );
 
         return factory;
     }
 
+
     @Bean
-    public DefaultErrorHandler kafkaErrorHandler() {
+    public DefaultErrorHandler kafkaErrorHandler(
+            KafkaTemplate<String, IssueEvent> kafkaTemplate
+    ) {
+
+        DeadLetterPublishingRecoverer recoverer =
+                new DeadLetterPublishingRecoverer(
+                        kafkaTemplate
+                );
 
         FixedBackOff fixedBackOff =
                 new FixedBackOff(
@@ -95,6 +115,7 @@ public class KafkaConsumerConfig {
                 );
 
         return new DefaultErrorHandler(
+                recoverer,
                 fixedBackOff
         );
     }
